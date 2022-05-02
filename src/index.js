cf20c5ab595280e9f6dea03c249d6e36b6d1911c
Rhiad <rhiad.ciccoli@gmail.com>
2022-05-02 12:42:55 -0300

    change ci

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ const app =  require( "./app");
 const port = process.env.PORT || 3000;
 
 app.listen(port, () =>
-  console.log('Example app listening on port 3000!'),
+  console.log('Example app listening on port 3000s!'),
 );
